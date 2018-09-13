358c1c2265720d6fa3e8a5118d50482d9576fa34
christine gomez <christine.gomez1226@gmail.com>
2018-09-13 22:09:09 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,17 +1,5 @@
-def make_word_hash(word_array)
-   word_hash = {}
-   word_array.each do |word|
-      word_hash[word] = []
-   end
- word_hash
- end
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
-   a_to_z_hash = alpha_hash(alphabet)
-   word_hash = make_word_hash(arr)
-   word_hash = set_word_hash_values(a_to_z_hash, word_hash)
-   words = sort_words(word_hash)
 
-   end
 end
