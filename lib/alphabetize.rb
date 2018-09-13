cb44a401d1e52f0ec29a77eaee1e60b8786ecc7e
christine gomez <christine.gomez1226@gmail.com>
2018-09-13 22:05:28 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,11 +2,4 @@ def alphabetize(arr)
   # code here
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
- a_to_z_hash = alpha_hash(alphabet)
- word_hash = make_word_hash(arr)
- word_hash = set_word_hash_values(a_to_z_hash, word_hash) 
- words = sort_words(word_hash)
-
- end
-
 end
