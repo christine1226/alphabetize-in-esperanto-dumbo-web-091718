1f4168c33460a09d6bfbef4f63b0e4b1e161a618
christine gomez <christine.gomez1226@gmail.com>
2018-09-13 22:11:55 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,10 @@
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-
+  arr.sort_by do |word|
+      word.split('').collect do |letter|
+        esperanto_alphabet.index(letter)
+      end
+    end
+  end
 
 end
